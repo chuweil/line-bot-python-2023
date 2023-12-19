bf9f17e6ea1ec60208a48405ad3e40e1d835e0da
henry <henryboy753951@gmail.com>
2023-12-19 22:00:49 +0800

    Update route registration in index.py

--- index.py
+++ index.py
@@ -9,9 +9,9 @@ from api.echo import route as echo_route
 # 如果以主程式執行，__name__ 會是 __main__
 # 如果是被引用，__name__ 會是模組名稱（也就是檔案名稱）
 app = Flask(__name__)
-# 註冊另一個模組 chat.py 的藍圖
-# 註冊後，chat.py 中的路由就會生效
-# 但是 chat.py 中的路由前面都會有 chat/ 的前綴 (url_prefix)
+# 註冊其他模組的路由
+# 註冊後，chat.py, keyword.py, echo.py 中的路由就會生效
+# 例如 chat.py 中的 /... 就會對應到網址 /chat/... (因為註冊時指定了 url_prefix='/chat')
 app.register_blueprint(chat_route,url_prefix='/chat')
 app.register_blueprint(keyword_route,url_prefix='/keyword')
 app.register_blueprint(echo_route,url_prefix='/echo')
